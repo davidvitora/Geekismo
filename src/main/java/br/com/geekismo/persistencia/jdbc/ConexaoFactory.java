9d7c88d34213a9cea9a8d85be701225547b3e0c2
kahary <david.vitora@gmail.com>
2015-10-23 18:33:43 -0200

    Url com variavel

--- src/main/java/br/com/geekismo/persistencia/jdbc/ConexaoFactory.java
+++ src/main/java/br/com/geekismo/persistencia/jdbc/ConexaoFactory.java
@@ -14,12 +14,16 @@ import java.sql.SQLException;
 class ConexaoFactory {
     
     public static Connection getConnection() throws ClassNotFoundException{
+     String  host = System.getenv("OPENSHIFT_MYSQL_DB_HOST");
+     String  port =  System.getenv("OPENSHIFT_MYSQL_DB_PORT");
+     String user = System.getenv("OPENSHIFT_MYSQL_DB_USERNAME");
+     String pw = System.getenv("OPENSHIFT_MYSQL_DB_PASSWORD");        
         try{
             Class.forName("com.mysql.jdbc.Driver"); // essa linha pode resolver o problema
             return DriverManager.getConnection(
                     /*"jdbc:mysql://localhost:3306/geekismo", "root", "potato371105");*/
                     /**"jdbc:mysql://$OPENSHIFT_MYSQL_DB_HOST:$OPENSHIFT_MYSQL_DB_PORT/appgeekismo", "adminuk4CVzm", "K71-t1TPLsf4");**/
-                   "jdbc:mysql://${env.OPENSHIFT_MYSQL_DB_HOST}:${env.OPENSHIFT_MYSQL_DB_PORT}/geekismo", "adminj2Mbgwe", "naKkUM1y_uyA");
+                   "jdbc:mysql://" + host +":"+ port + "/geekismo", user , pw);
         } catch (SQLException e) {
             throw new RuntimeException(e);}
     }
